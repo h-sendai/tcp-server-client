8a6b2ee8a82bcb073ce48f5fecba313c9471ee4a
Hiroshi Sendai <hiroshi.sendai@kek.jp>
2021-06-09 16:13:52 +0900

    pull socketkit upstream in client program

--- client/accept_connection.h
+++
@@ -1,7 +0,0 @@
-#ifndef _ACCEPT_CONNECTION
-#define _ACCEPT_CONNECTION 1
-
-extern int accept_connection(int port);
-
-#endif
-
--- client/my_socket.c
+++ client/my_socket.c
@@ -10,10 +10,61 @@ int udp_socket(void)
 	return socket(AF_INET, SOCK_DGRAM, 0);
 }
 
-int connect_tcp_timeout(int sockfd, char *ip_address, int port, int timeout)
+int connect_tcp_timeout(int sockfd, char *host, int port, int timeout_sec)
 {
-	fprintf(stderr, "not yet implement\n");
-	exit(1);
+	struct sockaddr_in servaddr;
+	struct sockaddr_in *resaddr;
+	struct addrinfo    hints;
+	struct addrinfo    *res;
+	int err;
+    struct timeval tm_out, orig_tm_out;
+    socklen_t len;
+
+    len = sizeof(orig_tm_out);
+    if (getsockopt(sockfd, SOL_SOCKET, SO_SNDTIMEO, &orig_tm_out, &len) < 0) {
+        warn("getsockopt() for restore SO_SNDTIMEO");
+        return -1;
+    }
+
+	res = 0;
+	memset((char *)&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = 0;
+	if ( (err = getaddrinfo(host, 0, &hints, &res)) != 0) {
+		return -1;
+	}
+
+	resaddr = (struct sockaddr_in *)res->ai_addr;
+	memset((char *)&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port   = htons(port);
+	servaddr.sin_addr   = resaddr->sin_addr;
+	freeaddrinfo(res);
+
+    tm_out.tv_sec  = timeout_sec;
+    tm_out.tv_usec = 0;
+
+    if (setsockopt(sockfd, SOL_SOCKET, SO_SNDTIMEO, &tm_out, sizeof(tm_out)) < 0) {
+        warnx("socket SO_SNDTIMEO timeout set fail");
+        return -1;
+    }
+    if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        /* warn("connect"); */
+        if (setsockopt(sockfd, SOL_SOCKET, SO_SNDTIMEO, &orig_tm_out, sizeof(orig_tm_out)) < 0) {
+            warn("setsockopt to restore SO_SNDTIMEO");
+        }
+        errno = ETIMEDOUT;
+        return -1;
+    }
+
+    /* restore SO_SNDTIMEO */
+    if (setsockopt(sockfd, SOL_SOCKET, SO_SNDTIMEO, &orig_tm_out, sizeof(orig_tm_out)) < 0) {
+        warn("setsockopt to restore SO_SNDTIMEO");
+        return -1;
+    }
+
+	return 0;
 }
 
 /* from kolc */
@@ -83,6 +134,8 @@ int connect_udp(int sockfd, char *host, int port)
 
 int my_bind(int sockfd, char *host, int port)
 {
+    /* specify host "0.0.0.0" to specify INADDR_ANY */
+
     struct sockaddr_in addr;
 
     memset(&addr, 0, sizeof(addr));
@@ -129,6 +182,13 @@ int set_so_rcvbuf(int sockfd, int so_rcvbuf)
     }
 
     ret_so_rcvbuf = get_so_rcvbuf(sockfd);
+#ifdef __linux__
+    if (ret_so_rcvbuf != (2*so_rcvbuf)) {
+#else
+    if (ret_so_rcvbuf != so_rcvbuf) {
+#endif
+        warnx("cannot set to %d bytes, but set %d bytes", so_rcvbuf, ret_so_rcvbuf);
+    }
 
     return ret_so_rcvbuf;
 }
@@ -178,4 +238,92 @@ int set_so_nodelay(int sockfd)
     }
 
     return 0;
+}
+
+int get_so_rcvlowat(int sockfd)
+{
+    int size;
+    socklen_t len;
+
+    if (getsockopt(sockfd, SOL_SOCKET, SO_RCVLOWAT , &size, &len) < 0) {
+        warn("getsockopt so_rcvlowat");
+        return -1;
+    }
+
+    return size;
+}
+
+int set_so_rcvlowat(int sockfd, int lowat)
+{
+    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVLOWAT , &lowat, sizeof(lowat)) < 0) {
+        warn("setsockopt so_rcvlowat");
+        return -1;
+    }
+
+    return 0;
+}
+
+int tcp_listen(int port)
+{
+    int on = 1;
+    int listenfd;
+    struct sockaddr_in serv_addr;
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port        = htons(port);
+
+    listenfd = tcp_socket();
+
+    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0) {
+        warn("setsockopt");
+        return -1;
+    }
+
+    if (bind(listenfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        warn("bind");
+        return -1;
+    }
+
+    if (listen(listenfd, 10) < 0) {
+        warn("listen");
+        return -1;
+    }
+    
+    return listenfd;
+}
+
+int accept_connection(int port)
+{
+    struct sockaddr_in remote_addr;
+    socklen_t addr_len = sizeof(remote_addr);
+
+    int sockfd, listenfd;
+
+    listenfd = tcp_listen(port);
+    if (listenfd < 0) {
+        fprintf(stderr, "tcp_listen");
+        return -1;
+    }
+    
+    sockfd = accept(listenfd, (struct sockaddr *) &remote_addr, &addr_len);
+    if (sockfd < 0) {
+        warn("accept");
+        return -1;
+    }
+
+    return sockfd;
+}
+
+int get_port_num(int sockfd)
+{
+    struct sockaddr_in myaddr;
+    socklen_t len = sizeof(myaddr);
+    if (getsockname(sockfd, (struct sockaddr *)&myaddr, &len) < 0) {
+        warn("getsockname");
+        return -1;
+    }
+
+    return ntohs(myaddr.sin_port);
 }
